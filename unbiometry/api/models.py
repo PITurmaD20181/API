d6512340e7834d9b78646de563b611d3ef4c03e4
Vinicius Ferreira <vfbdelima@gmail.com>
2018-06-05 21:59:03 -0300

    Creating models
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>
    Co-authored-by: Natália Rodrigues <nataliamrq@gmail.com>
    Co-authored-by: Gustavo Moreira <gustavomn93@gmail.com>
    Co-authored-by: joaorobson <joaorobsonmartins@gmail.com>
    Co-authored-by: cjjcastro <cjj.castro3@gmail.com>

--- unbiometry/api/models.py
+++ unbiometry/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Discipline(models.Model):
+
+    name = models.CharField(max_length=100, blank=False)
+    code = models.CharField(max_length=50, blank=False)
+
+
+class Class(models.Model):
+
+    name = models.CharField(max_length=10, blank=False)
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+
+    name = models.CharField(max_length=100, blank=False)
+    registration = models.CharField(max_length=11, blank=False)
+    classes = models.ManyToManyField(Class)
